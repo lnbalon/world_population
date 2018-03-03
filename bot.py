d67ac2f2bbb850a6bd233744cc502dd3c72b7066
lnbalon <lnbalon87@gmail.com>
2018-03-03 14:47:25 +0800

    Added new features to the bot.py file

--- bot.py
+++ bot.py
@@ -8,5 +8,13 @@ def square():
     for i in range(10):
         print('the square of {} is {}'.format(i, i**2))
 
+
+def cubed():
+
+    for i in range(10):
+        print('the cubed of {} is {}'.format(i, i**3))
+
+
 if __name__ == "__main__":
     square()
+    cubed()
